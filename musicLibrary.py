5d9ef51de4b7eaa0663d93692a62c9c52d2e5375
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-04-18 11:56:59 +0530

    new file

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "perfect": "https://youtu.be/2Vv-BfVoq4g?si=huCYp9bd7518B5Mb",
+    "snap": "https://youtu.be/Lo4_K4relMg?si=rGo3q1_dRRkacT-g",
+    "mockingbird": "https://youtu.be/S9bCLPwzSC0?si=MarS4HmQUA5KCp2Y",
+    "david": "https://youtu.be/ax8ffdMFsKs?si=A_blvDiFRKJnJop5",
+    "friend": "https://youtu.be/RgKAFK5djSk?si=xTQqp9PS3kkcuG2D",
+}

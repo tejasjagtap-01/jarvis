a7695ff6bd1afd5d7a389591daac820fe6029901
tejasjagtap-01 <tejasjagtap9977@gmail.com>
2026-05-05 16:26:35 +0530

    The CopyRight Free files

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,7 +1,5 @@
 music = {
-    "perfect": "https://youtu.be/2Vv-BfVoq4g?si=huCYp9bd7518B5Mb",
-    "snap": "https://youtu.be/Lo4_K4relMg?si=rGo3q1_dRRkacT-g",
-    "mockingbird": "https://youtu.be/S9bCLPwzSC0?si=MarS4HmQUA5KCp2Y",
-    "david": "https://youtu.be/ax8ffdMFsKs?si=A_blvDiFRKJnJop5",
-    "friend": "https://youtu.be/RgKAFK5djSk?si=xTQqp9PS3kkcuG2D",
+    "perfect": "https://youtu.be/yIF56YN5M_Q?si=IHSoFWC_jdyg00W5",
+    "snap": "https://youtu.be/p5cWMxzzMdA?si=gP5Y2lqXNmxx8qXH",
+
 }
